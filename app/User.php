4195fcda419661d65ee97fbe55d190a0673f7bbf
Shamim Hossen <shamim41.cse@gmail.com>
2020-06-30 16:47:45 +0600

    User model updated

--- app/User.php
+++ app/User.php
@@ -36,4 +36,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function discussions()
+    {
+        return $this->hasMany('App\Discussion');
+    }
 }
