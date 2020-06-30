234283c2b9eee34c4214830ca5dd9b5c203bbc2b
Shamim Hossen <shamim41.cse@gmail.com>
2020-06-30 16:48:11 +0600

    Route Updated

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
